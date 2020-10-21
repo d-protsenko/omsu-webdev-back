35aa986a36cdee6156df8a18ae34e7cd4c5da240
Danil Protsenko <dan.protsenko@gmail.com>
2020-10-21 20:02:52 +0600

    Fixed logging info form model.

--- src/main/kotlin/omsu/webdev/backend/api/models/forms/LoggingInfoForm.kt
+++ src/main/kotlin/omsu/webdev/backend/api/models/forms/LoggingInfoForm.kt
@@ -3,7 +3,6 @@ package omsu.webdev.backend.api.models.forms
 import java.time.ZonedDateTime
 
 class LoggingInfoForm(
-        var threads: Int? = null,
         var message: String? = null,
         var updatedAt: ZonedDateTime? = null
 )
